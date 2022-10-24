8dc061d2c5101d0d83f0438ab7b128e20eefd964
cemgthedev <cemg.the.dev@gmail.com>
2022-10-23 23:01:29 -0300

    adição do projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./style/global.css"
+import { Button } from './components/Button';
+import { Checkbox } from './components/Checkbox';
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+import { SignIn } from "./pages/SignIn";
+
+export function App() {
+  return (
+    <SignIn/>
+  );
+
+}
